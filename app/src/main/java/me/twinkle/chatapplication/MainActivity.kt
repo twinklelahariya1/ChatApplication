1a5998b0b853df88dfc1a7070da9f666d86c0e73
twinklelahariya1 <twinklelahariya1@gmail.com>
2022-03-06 12:10:57 +0530

    Add Database configuration to add user to the Firebase Db and add recyclerVIew to the main activity

--- app/src/main/java/me/twinkle/chatapplication/MainActivity.kt
+++ app/src/main/java/me/twinkle/chatapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package me.twinkle.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter : UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
     }
 }
--- app/src/main/java/me/twinkle/chatapplication/User.kt
+++ app/src/main/java/me/twinkle/chatapplication/User.kt
@@ -2,9 +2,9 @@ package me.twinkle.chatapplication
 
 class User {
 
-    private var name: String? = null
-    private var email: String? = null
-    private var password: String? = null
+    var name: String? = null
+    var email: String? = null
+    var password: String? = null
 
 
     constructor(name: String?, email: String?, password: String?) {

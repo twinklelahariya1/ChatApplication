99a9004d226b99b7efa71b630e7d101076db7089
twinklelahariya1 <twinklelahariya1@gmail.com>
2022-03-06 11:27:17 +0530

    Add event to signup and login a user into the application

--- app/src/main/java/me/twinkle/chatapplication/Login.kt
+++ app/src/main/java/me/twinkle/chatapplication/Login.kt
@@ -5,26 +5,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
     private lateinit var edtEmail: EditText
     private lateinit var edtPassword: EditText
-    private lateinit var buttonLogin: Button
+    private lateinit var btnLogin: Button
     private lateinit var btnSignup: Button
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
+
         edtEmail = findViewById(R.id.email)
         edtPassword = findViewById(R.id.password)
         btnSignup = findViewById(R.id.button_signup)
-        buttonLogin = findViewById(R.id.button_login)
+        btnLogin = findViewById(R.id.button_login)
 
-        btnSignup.setOnClickListener(){
+        btnSignup.setOnClickListener{
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+        btnLogin.setOnClickListener{
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email,password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful){
+                    val intent  = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this@Login, "User or password incorrect", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }

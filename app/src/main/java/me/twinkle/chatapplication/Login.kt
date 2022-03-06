f0cdd264418774d21fefb582580a4b31c81136ca
twinklelahariya1 <twinklelahariya1@gmail.com>
2022-03-06 09:55:25 +0530

    Add on click event on signup button from login page that redirects to signup page

--- app/src/main/java/me/twinkle/chatapplication/Login.kt
+++ app/src/main/java/me/twinkle/chatapplication/Login.kt
@@ -1,11 +1,30 @@
 package me.twinkle.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var buttonLogin: Button
+    private lateinit var btnSignup: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        edtEmail = findViewById(R.id.email)
+        edtPassword = findViewById(R.id.password)
+        btnSignup = findViewById(R.id.button_signup)
+        buttonLogin = findViewById(R.id.button_login)
+
+        btnSignup.setOnClickListener(){
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
